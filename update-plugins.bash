31dda004cdf7eb86b3edd7690638bf14adc4d062
Guillaume Lederrey <glederrey@wikimedia.org>
2017-03-03 11:18:40 +0100

    automate management of elasticsearch plugin repository
    This uses maven itself to download the required jars from Maven central,
    verify their GPG signatures against a list of trusted keys and upload them
    to our archiva repository.
    
    The elasticsearch plugins themselves are not hosted on Maven central and
    their Maven metadata is not available. They are downloaded from
    https://artifacts.elastic.co instead and no GPG check is performed.
    
    The deployment repo is updated with the version of the plugins that is
    configured in the pom.xml. The changes are not committed automatically.
    
    Change-Id: I4951607ee83901496baadbfcfb69f5be5f3248c8

---
+++ update-plugins.bash
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+set -e
+
+GIT_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ELASTICSEARCH_VERSION=`grep '<elasticsearch.version>' ${GIT_REPO}/pom.xml | sed -e 's/.*<elasticsearch.version>\(.*\)<\/elasticsearch.version>.*/\1/'`
+if [ -z "$ELASTICSEARCH_VERSION" ]; then
+  echo "No elastic version defined in $GIT_REPO/pom.xml";
+  exit 1;
+fi
+
+ELASTIC_PLUGINS="analysis-icu analysis-stempel"
+
+temp_dir=$(mktemp -d)
+
+echo "using ${temp_dir} as temporary directory for downloads"
+
+function cleanup {
+  echo "removing temporary directory ${temp_dir}"
+  rm -rf ${temp_dir}
+}
+trap cleanup EXIT
+
+function show_help {
+  cat << EOF
+Usage: ${0##*/} [upload-archiva|prepare-commit]
+
+upload-archiva: downloads plugin .zip files from https://artifacts.elastic.co
+                and upload them to archiva
+prepare-commit: prepares a commit with the updated plugins for the version of
+                elasticsearch defined in pom.xml
+
+===============================================================================
+*prerequisites*: the pom.xml must be updated with the correct elasticsearch
+                 version and the correct plugin dependencies
+
+EOF
+  exit 1
+}
+
+function install_elastic_plugins_locally {
+  for plugin in ${ELASTIC_PLUGINS}; do
+    plugin_filename=${plugin}-${ELASTICSEARCH_VERSION}.zip
+    curl -o ${temp_dir}/${plugin_filename} \
+      https://artifacts.elastic.co/downloads/elasticsearch-plugins/${plugin}/${plugin_filename}
+    ./mvnw install:install-file \
+      -Dfile=${temp_dir}/${plugin_filename} \
+      -DgroupId=org.elasticsearch.plugin \
+      -DartifactId=${plugin} \
+      -Dversion=${ELASTICSEARCH_VERSION} \
+      -Dpackaging=zip \
+      -DgeneratePom=true
+    unzip ${temp_dir}/${plugin_filename} -d ${temp_dir}
+    ./mvnw install:install-file \
+      -Dfile=${temp_dir}/elasticsearch/${plugin}-${ELASTICSEARCH_VERSION}.jar \
+      -DgroupId=org.elasticsearch.plugin \
+      -DartifactId=${plugin} \
+      -Dversion=${ELASTICSEARCH_VERSION} \
+      -Dpackaging=jar \
+      -DgeneratePom=true
+    rm -rf ${temp_dir}/elasticsearch
+  done
+}
+
+function deploy_jars_to_archiva {
+  ./mvnw -Dmdep.copyPom=true -DincludeScope=runtime clean dependency:copy-dependencies
+
+  echo "The following files are going to be uploaded to archiva"
+  ls target/dependency
+  echo "press [Ctrl]-[C] to abort or [Enter] to continue..."
+  read
+
+  for pom in target/dependency/*.pom; do
+    ./mvnw deploy:deploy-file \
+      -DrepositoryId=archiva.wikimedia.org \
+      -Durl=https://archiva.wikimedia.org/repository/mirrored \
+      -Dfile="${pom%%.pom}.jar" \
+      -DgeneratePom=false \
+      -DpomFile="${pom}"
+  done
+}
+
+function check_jar_on_archiva {
+  sha1=`sha1sum $1 | cut -d' ' -f1`
+  jar_name=`basename $1`
+  echo "Checking ${jar_name} with SHA1 ${sha1}"
+  link_dest=`rsync -an rsync://archiva.wikimedia.org/archiva/git-fat/${sha1} | sed -e 's/.* -> \(.*\)/\1/'`
+  if [ -z ${link_dest} ]; then
+    echo $1 not found in archiva.wikimedia.org
+    exit 1
+  fi
+  archiva_jar_name=`basename ${link_dest}`
+  if [ x${archiva_jar_name} != x${jar_name} ]; then
+    echo "${archiva_jar_name} does not match jar name ${jar_name}"
+    exit 1;
+  fi;
+}
+
+function update_git_deployment_repo {
+  ./mvnw -DoutputDirectory=target/plugins clean dependency:copy
+
+  for plugin in `ls ${GIT_REPO}/target/plugins`; do
+    rm -rf ${temp_dir}/plugin
+    mkdir -p ${temp_dir}/plugin
+    unzip ${GIT_REPO}/target/plugins/${plugin} -d ${temp_dir}/plugin
+    plugin_desc=${temp_dir}/plugin/elasticsearch/plugin-descriptor.properties
+    plugin_es_version=`grep '^elasticsearch.version=' ${plugin_desc}`
+    plugin_es_version=${plugin_es_version##*=}
+    plugin_version=`grep '^version=' ${plugin_desc}`
+    plugin_version=${plugin_version##*=}
+    echo Found matching es version ${plugin_es_version}
+    if [ ${plugin_es_version} != ${ELASTICSEARCH_VERSION} ]; then
+      echo ${plugin} does not match elastic version ${ELASTICSEARCH_VERSION}
+      exit 1
+    fi
+    for jar in `find ${temp_dir}/plugin/elasticsearch -name '*.jar'`; do
+      check_jar_on_archiva ${jar}
+    done
+    name=${plugin##*/}
+    name=`basename ${name} -${plugin_version}.zip`
+    mkdir -p ${temp_dir}/deploy/${name}
+    cp -a ${temp_dir}/plugin/elasticsearch/. ${temp_dir}/deploy/${name}
+  done
+
+  for deployed in `find ${temp_dir}/deploy -maxdepth 1 -mindepth 1 -type d`; do
+    echo ${deployed}
+    name=`basename ${deployed}`
+    if [ -d ${GIT_REPO}/${name} ]; then
+      git rm -r ${GIT_REPO}/${name}
+    fi
+    mkdir -p ${GIT_REPO}/${name}
+    cp -a ${deployed}/. ${GIT_REPO}/${name}
+    git add ${GIT_REPO}/${name}
+  done
+}
+
+command="$1"
+case ${command} in
+  upload-archiva)
+  install_elastic_plugins_locally
+  ./mvnw clean pgpverify:check
+  deploy_jars_to_archiva
+  ;;
+
+  prepare-commit)
+  ./mvnw clean pgpverify:check
+  update_git_deployment_repo
+  ;;
+
+  *)
+  show_help
+  ;;
+esac
